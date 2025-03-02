6830ba3902279d420336aa22d0d900147251af92
Aman Kumar <authorisedaman@gmail.com>
2025-03-02 20:32:00 +0530

    added

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,7 +1,8 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  VITE_BASE_URL: string;
+  readonly VITE_BASE_URL: string;
+  readonly VITE_SOCKET_URL: string;
 }
 
 interface ImportMeta {
