a3f47d7935b4d05250ab63e54b4f8437bb0d2eb1
Aman Kumar <authorisedaman@gmail.com>
2025-03-02 20:21:12 +0530

    added -3

--- client/src/api/axiosInstance.ts
+++ client/src/api/axiosInstance.ts
@@ -1,5 +1,9 @@
 import axios from "axios";
 
+interface ImportMetaEnv {
+  VITE_API_URL: string;
+}
+
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
   withCredentials: true,
